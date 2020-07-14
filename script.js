b0a3d6a12b8727f1a45cd406914e44ea145f9147
Jakub Krukowski <62674442+KrukowskiJ@users.noreply.github.com>
2020-07-14 16:25:44 +0200

    make navbar responsive add hamburger menu/animation to mobile version

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-jQuery(window).load(function() {
-  jQuery(window).bind('scroll', function() {
-  var navHeight = jQuery( window ).height() - 90;
-    if (jQuery(window).scrollTop() > navHeight) {
-      jQuery('nav').addClass('fixed');
-    }
-    else {
-      jQuery('nav').removeClass('fixed');
-    }
- });
+jQuery(window).bind('scroll', function() {
+var navHeight = jQuery( window ).height() - 90;
+  if (jQuery(window).scrollTop() > navHeight) {
+    jQuery('nav').addClass('fixed');
+  }
+  else {
+    jQuery('nav').removeClass('fixed');
+  }
 });
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    //animate navLinks
+    navLinks.forEach((link,index) => {
+    if(link.style.animation){
+    link.style.animation = '';
+  }
+    else{
+           link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +1}s`;
+    }
+  });
+
+  //burger animation
+  burger.classList.toggle('toggle');
+});
+}
+
+navSlide();
